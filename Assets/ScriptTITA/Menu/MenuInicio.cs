174ee65964f5ad90beb2ff7e6f2130ece8129f2c
S246now <rab.guez1963@gmail.com>
2024-05-08 14:20:49 -0500

    New menu with interactions ray and poke (probably just with the headset)

--- Assets/ScriptTITA/Menu/MenuInicio.cs
+++ Assets/ScriptTITA/Menu/MenuInicio.cs
@@ -5,11 +5,40 @@ using UnityEngine.SceneManagement; // Agrega el espacio de nombres necesario
 
 public class MenuInicio : MonoBehaviour
 {
-    // No es necesario definir Start() y Update() si no los estás utilizando
+    [SerializeField] private PrimitiveType[] primitiveTypes = { PrimitiveType.Cube, PrimitiveType.Sphere, PrimitiveType.Cylinder };
 
-    // Este método cambiará de escena cuando se llame desde un botón
-    public void CambiarScena(string nombre)
+    [SerializeField] private Vector3 spawnPosition = new Vector3(1, 1, 2);
+    [SerializeField] private Vector3 spawnRotation = new Vector3(45, 45, 0);
+    [SerializeField] private Vector3 spawnScale = new Vector3(0.5f, 0.5f, 0.5f);
+
+    private int currentIndex = -1;
+    private bool hasBeenGenerated;
+    private List<GameObject> objs = new List<GameObject>();
+    public void GenerateObjects()
     {
-        SceneManager.LoadScene(nombre); // El nombre del método es "LoadScene", no "Manager"
+        if (currentIndex >= 0)
+        {
+            return;
+        }
+
+        GameObject container = new GameObject("ObjectsContainer");
+        for (int i = 0; i < primitiveTypes.Length; i++)
+        {
+            GameObject obj = GameObject.CreatePrimitive(primitiveTypes[i]);
+            obj.transform.parent = container.transform;
+            obj.transform.position = spawnPosition;
+            obj.transform.rotation = Quaternion.Euler(spawnRotation);
+            obj.transform.localScale = spawnScale;
+            obj.SetActive(false);
+            objs.Add(obj);
+        }
+        currentIndex = 0;
+        objs[currentIndex].SetActive(true);
+        hasBeenGenerated = true;
+    }
+
+    public void CambiarScena(int numEscena)
+    {
+        SceneManager.LoadScene(numEscena);
     }
 }
